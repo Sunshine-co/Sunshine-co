df6dd6cea90048677e9bf93d21479dfa42a17c0b
Samy <sara2753@hotmail.com>
2022-10-21 00:15:44 -0500

    actualizacion from

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,101 +1,61 @@
 import React, { Fragment, useEffect } from 'react'
 import MetaData from './layout/MetaData'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { getProducts } from '../actions/productActions'
+import { Link } from 'react-router-dom'
+import { useAlert} from 'react-alert'
 
 export const Home = () => {
+    const { loading, productos, error} = useSelector(state=> state.products)
+    const alert= useAlert();
+
     const dispatch = useDispatch();
     useEffect(() => {
+        if (error){
+            return alert.error(error)
+        }
+
         dispatch(getProducts());
+        alert.success("OK")
     }, [dispatch])
-    
-    
+
+
     return (
         <Fragment>
-          <MetaData title="Lo mejor para tu belleza"></MetaData>
+            {loading ? <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i> :(
+                <Fragment>
+                    <MetaData title="Lo mejor para tu belleza"></MetaData>
             <h1 id="encabezado_productos">Ultimos Productos</h1>
 
             <section id="productos" className='container mt-5'>
                 <div className='row'>
-                    {/*Producto 1*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                    {productos && productos.map (producto => (
+                        <div key={producto._id} className='col-sm-12 col-md-6 col-lg-3 my-3'>
                         <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/Sombras_james.png' alt=" James"></img>
+                            <img className='card-img-top mx-auto' src={producto.imagen[0].url} alt={producto.imagen[0].public_id}></img>
                             <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Sombras james</a></h5>
+                                <h5 id="titulo_producto"><Link to={`/producto/${producto._id}`}>{producto.nombre}</Link></h5>
                                 <div className='rating mt-auto'>
                                     <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
+                                        <div className='rating-inner' style={{width: `${(producto.calificacion/5)*100}%`}}></div>
                                     </div>
-                                    <span id="No_de_opiniones"> 5 reviews</span>
+                                    <span id="No_de_opiniones"> {producto.numCalificaciones} Reviews</span>
                                 </div>
-                                <p className='card-text'>$30.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
+                                <p className='card-text'>${producto.precio}</p><Link to={`/producto/${producto._id}`} id="view_btn" className='btn btn-block'>
                                     Ver detalle
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
 
-                    {/*Producto 2*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/Sombras_Mac.png' alt="Sombras Mac "></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Sombras Mac</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 2 reviews</span>
-                                </div>
-                                <p className='card-text'>$40.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
+                    ))}
                     </div>
-
-                    {/*Producto 3*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/Sombras_Naked.png' alt="Sombras Naked"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Sombras Naked</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 12 reviews</span>
-                                </div>
-                                <p className='card-text'>$25.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/*Producto 4*/}
-                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src='./images/Sombras_Ultraviolet.png' alt="Naked Ultraviolet"></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Naked Ultraviolet</a></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner'></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> 7 reviews</span>
-                                </div>
-                                <p className='card-text'>$30.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
             </section>
+
+                </Fragment>
+
+            )}
+            
 
         </Fragment>
     )
