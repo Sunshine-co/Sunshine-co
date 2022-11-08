6a1996780b76eca685f77671031c0c01f951ba2b
Samy <sara2753@hotmail.com>
2022-11-08 14:21:29 -0500

    Busqueda productos

--- front/src/components/admin/ProductsList.js
+++ front/src/components/admin/ProductsList.js
@@ -9,7 +9,7 @@ import { getProducts } from '../../actions/productActions'
 import {Link } from "react-router-dom"
 
 export const ProductsList = () => {
-    const { loading, productos, error} = useSelector(state=> state.products)
+    const { loading, products, error} = useSelector(state=> state.products)
     const alert= useAlert();
 
     const dispatch = useDispatch();
@@ -52,7 +52,7 @@ export const ProductsList = () => {
             rows: []
         }
 
-        productos.forEach(product => {
+        products.forEach(product => {
             data.rows.push({
                 nombre: product.nombre,
                 precio: `$${product.precio}`,
